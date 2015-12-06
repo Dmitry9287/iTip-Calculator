d1bcc6906e05e212ecf3dc026dc9744ca4e3a29d
Lorenzo Zanotto <contact@lorenzozanotto.com>
2015-12-06 13:13:37 +0100

    Initial Commit

---
+++ iTip/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iTip
+//
+//  Created by Lorenzo Zanotto on 06/12/2015.
+//  Copyright © 2015 Lorenzo Zanotto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iTip/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iTip
+//
+//  Created by Lorenzo Zanotto on 06/12/2015.
+//  Copyright © 2015 Lorenzo Zanotto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
